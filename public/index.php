ad92010d35fb43470e9798ec7797470d3985f8f5
Alexandre Rangom <alexandre.rangom@gmail.com>
2023-04-08 12:55:07 +0200

    test(index): ajout des routes pour tester les fonctionnalités

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Containers\AppContainer;
+use App\Controllers\BooksController;
 use Slim\Factory\AppFactory;
 use \Psr\Http\Message\RequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
@@ -11,6 +12,13 @@ $appContainer = new AppContainer();
 
 AppFactory::setContainer($appContainer->getContainer());
 $app = AppFactory::create();
+
+$app->get("/book/{title}", BooksController::class . ":fetchBookDataByTitle");
+
+$app->get('/', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write("Hello Word !");
+    return $response;
+});
 
 $app->get('/{name}', function (Request $request, Response $response, array $args) {
     $response->getBody()->write("Hello Word !");
